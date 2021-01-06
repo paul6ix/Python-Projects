cd22baa6ca17a95404d1458efd4a2a9573c6e9e2
Paul6ix <okporp@gmail.com>
2021-01-06 11:44:42 +0100

    updated the dictionary to get close matches

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 # imported json file to load data for the dictionary
 
@@ -10,10 +11,28 @@ data = json.load(open("data.json"))
 # function logic to search for data
 def translate(word):
     # checks if the word exists
+    word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    # checking for close words
+    elif len(get_close_matches(word, data.keys())) > 0:
+        correct_word = get_close_matches(word, data.keys())[0]
+        print("Is this the correct word {}: ".format(correct_word))
+        next_step = input("Press \"Y\" to continue or \"N\" to stop ")
+        if next_step == "Y":
+            return data[correct_word]
+        elif next_step == "N":
+            print("This word does not exist")
+        else:
+            print("Press \"Y\" to continue or \"N\" to stop ")
+
+
     else:
-        print("The word does not exist")
+        print("This word does not exist")
 
 
 # accepting input for word from user
@@ -21,4 +40,11 @@ word = input("Enter a word to search: ")
 # passing it to the function
 output = translate(word)
 # giving output for searched word
-print(output)
+
+# Checking if word has multiple meanings
+if type(output) == list:
+    # printing out each meaning in a new line
+    for item in output:
+        print(item)
+else:
+    print(output)
